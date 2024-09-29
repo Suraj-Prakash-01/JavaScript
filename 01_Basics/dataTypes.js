c8b84147b2c36fe279c7d467a324b2953ad55978
Coder <surajprakash1949@gmail.com>
2024-09-29 21:57:25 +0530

    Data Types in JS

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,23 @@
+// Treat all JS code as newer version
+"use strict";
+
+// We are using node JS, not browser
+//alert("hello");
+
+let name = "Suraj";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// Number -> 2 to power 53
+// BigInt
+// String
+// Boolean -> true/false
+// NUll -> Standalone Value
+// Undefined -> When value is not defined by user
+// Symbol -> Unique and immutable value
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);
