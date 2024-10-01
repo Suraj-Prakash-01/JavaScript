5aed9cadff96170d0dd2fa780bc3bb67b77c781c
Coder <surajprakash1949@gmail.com>
2024-10-01 18:35:43 +0530

    Arrays

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,19 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeros = ["Shankitman", "Naagraj"];
+
+console.log(myArr[0]);
+
+// Array Methods
+
+myArr.push(6);
+myArr.pop();
+
+console.log(myArr.includes(9));
+console.log(myArr);
+
+const newArr = myArr.join();
+
+console.log(newArr);
+console.log(typeof newArr);
